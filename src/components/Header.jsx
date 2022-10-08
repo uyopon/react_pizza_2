2d85101a08fbcaf349bfd9beaac93cfce82cfb35
boss <andryousha228@gmail.com>
2022-10-08 21:33:44 +0500

    button

---
+++ src/components/Header.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import logoSvg from '../assets/img/pizza-logo.svg'
+import Button from './Button'
+
+function Header() {
+
+    return (
+        <div className="header">
+            <div className="container">
+                <div className="header__logo">
+                    <img width="38" src={logoSvg} />
+                    <div>
+                        <h1>React Pizza</h1>
+                        <p>самая вкусная пицца во вселенной</p>
+                    </div>
+                </div>
+                <div className="header__cart">
+
+                    <Button/>
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Header
+
+    
