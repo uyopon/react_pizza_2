6c490b45dd973460f898978e047e542666bfb086
boss <andryousha228@gmail.com>
2022-10-11 21:10:57 +0500

    categories complite

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,15 +1,24 @@
-import React from 'react'
+import React  from 'react'
 
-function Categories({items}) {
+function Categories({items, }) {
+    
+    const [activeItem, setActiveItem] = React.useState(null)
+
+    const onSelectItem=(value)=>{
+        setActiveItem(value)
+        
+    }
+    
     return (
+        
         <div className="categories">
+            
             <ul>
-                <li className="active">Все</li>
-                <li>Мясные</li>
-                <li>Вегетарианская</li>
-                <li>Гриль</li>
-                <li>Острые</li>
-                <li>Закрытые</li>
+                <li onClick ={()=>onSelectItem(null)} className={activeItem===null?'active':''} >Все</li>
+                
+                {items && items.map((name,index)=> <li  onClick={()=>onSelectItem(index)} className={activeItem===index?'active':''}
+                 key ={name}>{name}</li>)}
+
             </ul>
         </div>
     )
