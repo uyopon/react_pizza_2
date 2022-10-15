9ccbd120c78b59928173c319312eb073f729f74d
boss <andryousha228@gmail.com>
2022-10-15 18:01:55 +0500

    fetch

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import React from 'react';
 import Header from './components/Header'
-import Categories from './components/Categories'
-import SortPopup from './components/SortPopup';
 import Home from './pages/Home';
 import Cart from './pages/Cart'
 import {Routes, Route } from 'react-router-dom';
 
-
-
-
-
 function App(){
+  const  [pizzas , setPizzas] = React.useState([]) //пиццы с сервера
+  React.useState(()=>{
+    fetch('http://localhost:3000/db.json').then((resp)=>resp.json()).then((json)=>setPizzas(json.pizzas))
+    
+  },[])
+  
 
   return (
   <div className="wrapper">
     <Header />
     <div className="content">
       <Routes>
-        <Route exact path='/' element={<Home  />} />
+        <Route exact path='/' element={<Home pizzas= {pizzas}  />} />
         <Route path='cart' element={<Cart />} />
       </Routes>
     </div>
--- src/index.js
+++ src/index.js
@@ -26,14 +26,3 @@ root.render(
 
 
 
-{/* <BrowserRouter>
-      <div className="wrapper">
-        <Header />
-        <div className="content">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/cart" element={<Cart />} />
-          </Routes>
-        </div>
-      </div>
-    </BrowserRouter> */}
